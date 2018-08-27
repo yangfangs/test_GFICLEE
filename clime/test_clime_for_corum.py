f4fd47974114b3a565de141811b8f5ed2559e9cf
yangfang <506528950@qq.com>
2018-08-27 08:04:48 +0800

    test go and corum

--- clime/test_clime_for_corum.py
+++ clime/test_clime_for_corum.py
@@ -5,14 +5,15 @@ from clime.clime_predict import ClimePredict
 foo = ClimePredict()
 # 2-fold cross validation
 foo.leave_half('/home/yangfang/GFICLEE/test_corum/input/', '/home/yangfang/GFICLEE/test_corum/output/',
-               '/home/yangfang/GFICLEE/test_corum/human.KEGG.txt')
+               '/home/yangfang/GFICLEE/test_corum/human.corum.txt')
 # generate the clime parameters for test
 foo.get_prm('/home/yangfang/GFICLEE/test_corum/human_CI_half.prms',
             '//home/yangfang/GFICLEE/test_corum/prms_all/',
             '/home/yangfang/GFICLEE/test_corum/result_all/')
 start =time.time()
 # run test with 6 cores
-# foo.run_clime(6)
+# run test with 6 cores
+foo.run_clime(6, clime_path='/home/yangfang/tools/clime')
 end = time.time()
 print("===============================")
 print('Running time: {} Seconds'.format(end-start))
--- clime/test_clime_for_go.py
+++ clime/test_clime_for_go.py
@@ -5,14 +5,14 @@ from clime.clime_predict import ClimePredict
 foo = ClimePredict()
 # 2-fold cross validation
 foo.leave_half('/home/yangfang/GFICLEE/test_go/input/', '/home/yangfang/GFICLEE/test_go/output/',
-               '/home/yangfang/GFICLEE/test_go/human.KEGG.txt')
+               '/home/yangfang/GFICLEE/test_go/human.GO.txt')
 # generate the clime parameters for test
 foo.get_prm('/home/yangfang/GFICLEE/test_go/human_CI_half.prms',
             '//home/yangfang/GFICLEE/test_go/prms_all/',
             '/home/yangfang/GFICLEE/test_go/result_all/')
 start =time.time()
 # run test with 6 cores
-# foo.run_clime(6)
+foo.run_clime(6, clime_path='/home/yangfang/tools/clime')
 end = time.time()
 print("===============================")
 print('Running time: {} Seconds'.format(end-start))
