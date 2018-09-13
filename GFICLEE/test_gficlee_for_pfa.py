987dc85d12801acf1f5b870c4743ed451af881cb
yangfang <506528950@qq.com>
2018-09-13 17:49:58 +0800

    test gficlee

---
+++ GFICLEE/test_gficlee_for_pfa.py
@@ -0,0 +1,19 @@
+import time
+
+from GFICLEE.gficlee_predict import GficleePredict
+
+foo = GficleePredict(profile_path='/home/yangfang/GFICLEE/test_pfa_gficlee/pfa.matrix138.e3.q00.p20.txt',
+                     tree_path='/home/yangfang/GFICLEE/test_pfa_gficlee/species138.abbrev.manual_binary.nwk',
+                     result_path='/home/yangfang/GFICLEE/test_pfa_gficlee/result/')
+# 2-fold cross validation
+foo.leave_half('/home/yangfang/GFICLEE/test_pfa_gficlee/input/',
+               '/home/yangfang/GFICLEE/test_pfa_gficlee/output/',
+               '/home/yangfang/GFICLEE/test_pfa_gficlee/pfa.genome.txt')
+
+
+start =time.time()
+# run test with 6 cores
+foo.run_gficlee(6, gficlee_path='GFICLEE')
+end = time.time()
+print("===============================")
+print('Running time: {} Seconds'.format(end-start))
